637cc3c9a2cae9a96997c778016acdf2904366a5
emwhbr <hakanbrolin@hotmail.com>
2014-12-21 22:01:49 +0100

    Added PICkit 2 information

---
+++ pickit2/setup_pickit2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# /************************************************************************
+#  *                                                                      *
+#  * Copyright (C) 2014 Bonden i Nol (hakanbrolin@hotmail.com)            *
+#  *                                                                      *
+#  * This program is free software; you can redistribute it and/or modify *
+#  * it under the terms of the GNU General Public License as published by *
+#  * the Free Software Foundation; either version 2 of the License, or    *
+#  * (at your option) any later version.                                  *
+#  *                                                                      *
+#  ************************************************************************/
+
+PICKIT2_HOME=/opt/pickit2
+
+PATH=$PICKIT2_HOME/:$PATH
+
+export PATH
